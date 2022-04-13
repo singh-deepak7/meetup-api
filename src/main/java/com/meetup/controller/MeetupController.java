2438ac0d3cc973caf75b0faed3f86d444d69f92d
singh-deepak7 <singh-deepak@outlook.com>
2022-04-13 16:50:34 -0500

    added post to save data.

--- src/main/java/com/meetup/controller/MeetupController.java
+++ src/main/java/com/meetup/controller/MeetupController.java
@@ -1,11 +1,13 @@
 package com.meetup.controller;
 
+import javax.validation.Valid;
+
+import com.meetup.exception.ResourceNotFoundException;
 import com.meetup.model.Meetup;
 import com.meetup.repository.MeetupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +21,18 @@ public class MeetupController {
     @GetMapping("/meetup")
     public List<Meetup> getAllMeetups() {
         return meetupRepository.findAll();
+    }
+
+    @GetMapping("/meetup/{id}")
+    public ResponseEntity<Meetup> getEmployeeById(@PathVariable(value = "id") Long meetupId)
+            throws ResourceNotFoundException {
+        Meetup meetup = meetupRepository.findById(meetupId)
+                .orElseThrow(() -> new ResourceNotFoundException("Meetup not found for this id :: " + meetupId));
+        return ResponseEntity.ok().body(meetup);
+    }
+
+    @PostMapping("/meetup")
+    public Meetup createEmployee(@Valid @RequestBody Meetup meetup) {
+        return meetupRepository.save(meetup);
     }
 }
